dcd21c071f9c3a2d79eca8c8a6da513c638eaebe
peterlravn <37664574+peterlravn@users.noreply.github.com>
2024-05-29 00:49:34 +0200

    g

--- Code collected - CLEAN/+sticky_wages_both/+sparse/+block/dynamic_1.m
+++
@@ -1,5 +0,0 @@
-function [y, T] = dynamic_1(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(23)=params(9)*y(11)+x(2);
-  y(20)=y(23)*(1-params(5)*(1+params(2))/(params(5)+params(2)+(1-params(5))*params(1)))/(1-params(5));
-  y(22)=y(23)*(1+params(2))/(params(5)+params(2)+(1-params(5))*params(1))*(-params(1))*(1-params(9));
-end
--- Code collected - CLEAN/+sticky_wages_int/+sparse/+block/static_3.m
+++
@@ -1,4 +0,0 @@
-function [y, T] = static_3(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(8)=(1-params(5)*(1+params(2))/(params(5)+params(2)+(1-params(5))*params(1)))/(1-params(5))*y(11);
-  y(10)=y(11)*(1+params(2))/(params(5)+params(2)+(1-params(5))*params(1))*(-params(1))*(1-params(9));
-end
